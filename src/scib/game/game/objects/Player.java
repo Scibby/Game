5c46e812d0ff86565a3f6f2ac8227bfcab59f06e
Andrew <Andrew@Andrew-PC>
2016-03-18 19:23:50 +0100

    Version 6
    Player class created, includes the getters and setters created in
    Version 5

---
+++ src/scib/game/game/objects/Player.java
@@ -0,0 +1,58 @@
+package scib.game.game.objects;
+
+import java.util.LinkedList;
+
+import scib.game.framework.GameObject;
+import scib.game.framework.ObjectId;
+
+public class Player extends GameObject {
+
+	public Player(float x, float y, ObjectId id) {
+		super(x, y, id);
+	}
+
+	public void tick(LinkedList<GameObject> object) {
+
+	}
+
+	public void render() {
+
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getVelX() {
+		return velX;
+	}
+
+	public float getVelY() {
+		return velY;
+	}
+
+	public void setVelX(float velX) {
+		this.velX = velX;
+	}
+
+	public void setVelY(float velY) {
+		this.velY = velY;
+	}
+
+	public ObjectId getId() {
+		return null;
+	}
+
+}
