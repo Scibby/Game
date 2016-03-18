dc3d2539bac232ee9866e7ca27992bc28aaaadd7
Andrew <Andrew@Andrew-PC>
2016-03-18 18:52:33 +0100

    Version 2
    Game loop created, runnable, start(), stop() and run() are all working

--- src/scib/game/Game.java
+++ src/scib/game/Game.java
@@ -13,7 +13,37 @@ public class Game extends Canvas implements Runnable{
 	public static final int SCALE = 2;
 	public static final Dimension dimension = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
 	public final String TITLE = "2D Platformer";
+
+	private boolean running = false;
+	private Thread thread;
 	
+	private synchronized void start(){
+		if(running) return;
+		
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	private synchronized void stop(){
+		if(!running) return;
+		
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.exit(1);
+	}
+	
+	public void run() {
+		while(running){
+			
+		}
+		
+	}
+
 	public static void main(String[] args) {
 		Game game = new Game();
 
@@ -28,5 +58,7 @@ public class Game extends Canvas implements Runnable{
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		
+		game.start();
 	}
 }
